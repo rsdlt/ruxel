1aac1d0e581b2ea22fe6d434ef6f9505d19f8e27
RSDLT <rsdlt@pm.me>
2022-10-06 22:36:57 -0400

    identifying hits working

--- src/geometry/intersection.rs
+++ src/geometry/intersection.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use crate::shapes::*;
+use crate::shapes::{sphere::*, Shape};
 use num::{Num, NumCast};
 use std::fmt::Display;
 
@@ -24,19 +24,20 @@ mod tests;
 /// Macro that takes 'n' intersections and returns a collection 'Vec<Intxn>'.
 #[macro_export]
 macro_rules! intersections {
-    ( $( $ix: expr ),+ ,) => {
+    [ $( $ix: expr ),+ ,] => {
        vec![ $( $ix ),* ]
     };
 
-    ( $( $ix: expr ),+) => {
+    [ $( $ix: expr ),+] => {
        vec![ $( $ix ),* ]
     };
 }
 
+/// Type representing a collection of intersections.
 pub type IntxnVec<P, S> = Vec<Intxn<P, S>>;
 
 /// Type representing an intersection between a Ray and a Shapes.
-#[derive(Clone, Copy, Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct Intxn<P, S> {
     /// Distance 't' value of an intersection accross a Ray path.
     pub t: P,
@@ -55,11 +56,50 @@ where
     }
 }
 
+// Comparison based on 't'.
+impl<P, S> PartialEq for Intxn<P, S>
+where
+    P: Num + Copy,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.t == other.t
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
+
+/// Finds and returns the 'hit' -visible intersection- in a collection.
+pub fn hit<P, S>(xs: IntxnVec<P, S>) -> Option<Intxn<P, S>>
+where
+    P: Num + NumCast + Copy + PartialEq + PartialOrd,
+    S: Shape<P> + Copy,
+{
+    let mut min = xs[0].t;
+    let mut id = 0;
+    let mut flag = false;
+    for (idx, ixn) in xs.iter().enumerate() {
+        if ixn.t >= num::zero() {
+            if min > ixn.t {
+                min = ixn.t;
+                id = idx;
+            }
+            flag = true;
+        }
+    }
+    if !flag {
+        return None;
+    }
+
+    return Some(xs[id]);
+}
+
 /// Common set of operations for Intersections
 pub trait Intersection<P, S>
 where
     P: Num + Copy,
-    S: Shape<P>,
+    S: Shape<P> + Copy,
 {
     /// Returns an intersection with a 't' distance between a Ray and a Shape
     fn intersection(t: P, object: S) -> Intxn<P, S>;
@@ -68,7 +108,7 @@ where
 impl<P, S> Intersection<P, S> for Intxn<P, S>
 where
     P: Num + Copy,
-    S: Shape<P>,
+    S: Shape<P> + Copy,
 {
     fn intersection(t: P, object: S) -> Self {
         Self { t, object }
--- src/geometry/intersection/tests.rs
+++ src/geometry/intersection/tests.rs
@@ -33,3 +33,49 @@ fn ut_intersection_aggregating() {
     assert_eq!(xs[0].t, 1);
     assert_eq!(xs[1].t, 2);
 }
+
+#[test]
+// hit all intxns have positive t
+fn ut_intersection_hit_positive_t() {
+    let s = Sphere::new(1);
+    let i1 = Intxn::intersection(1, s);
+    let i2 = Intxn::intersection(2, s);
+    let xs = intersections![i2, i1];
+    let i = hit(xs);
+    assert_eq!(i, Some(i1));
+}
+
+#[test]
+// hit some intxns have negative t
+fn ut_intersection_hit_negative_t() {
+    let s = Sphere::new(1);
+    let i1 = Intxn::intersection(-1, s);
+    let i2 = Intxn::intersection(1, s);
+    let xs = intersections![i2, i1];
+    let i = hit(xs);
+    assert_eq!(i, Some(i2));
+}
+
+#[test]
+// hit all intxns have negative t
+fn ut_intersection_hit_negative_all_t() {
+    let s = Sphere::new(1);
+    let i1 = Intxn::intersection(-2, s);
+    let i2 = Intxn::intersection(-1, s);
+    let xs = intersections![i2, i1];
+    let i = hit(xs);
+    assert_eq!(i, None);
+}
+
+#[test]
+// hit is lowest non-negative inx
+fn ut_intersection_hit_lowest_t() {
+    let s = Sphere::new(1);
+    let i1 = Intxn::intersection(5, s);
+    let i2 = Intxn::intersection(7, s);
+    let i3 = Intxn::intersection(-3, s);
+    let i4 = Intxn::intersection(2, s);
+    let xs = intersections![i2, i1, i3, i4];
+    let i = hit(xs);
+    assert_eq!(i, Some(i4));
+}
